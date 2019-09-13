37cec662c7d3a72a93fe1f9572aaaf5af0fa54c6
Azad Gandomi <azad.gandomi@gmail.com>
2019-09-13 22:54:12 +0200

    Small fixes

--- app/src/main/java/com/sazadgankar/picontroller/Constants.kt
+++ app/src/main/java/com/sazadgankar/picontroller/Constants.kt
@@ -1,5 +1,8 @@
 package com.sazadgankar.picontroller
 
+// const val SERVICE_TYPE = "NSD Service type"
+// const val HOST_NAME = "Host Name"
+
 const val PORT_CONTROL = 56789
 const val PORT_CAMERA = 45678
 
